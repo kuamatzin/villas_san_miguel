a480ee8c1d80eaabb3c10225f94f4214d11dece6
Carlos Cuamatzin <kuamatzin@gmail.com>
2016-12-16 17:28:32 -0600

    Checar problema con params en request

--- resources/views/scripts/contacto.blade.php
+++ resources/views/scripts/contacto.blade.php
@@ -15,6 +15,7 @@
         methods: {
             contacto: function(){
                 var that = this;
+                alert(this.name)
                 this.$http.get('/enviar_email/?name=' + this.name + '&email=' + this.email + '&phone=' + this.phone + '&message=' + this.message + '&nss=' + this.nss + '&birthday=' + this.birthday).then(function(response){
                     if (response.body == 'Exito') {
                         alert("Enseguida nos pondremos en contacto contigo")
